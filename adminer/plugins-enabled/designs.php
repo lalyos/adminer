e7137f5d21c36700c92a036e0967ec19dfb92922
lalyos <lalyos@yahoo.com>
2020-10-19 18:06:24 +0200

    add design chooser bottom right

---
+++ adminer/plugins-enabled/designs.php
@@ -0,0 +1,16 @@
+<?php
+require_once('plugins/designs.php');
+
+return new AdminerDesigns(
+        $designs = array(
+			"https://raw.githack.com/pepa-linha/Adminer-Design-Dark/master/adminer.css"=>"pepa-linha-dark",
+			"https://raw.githack.com/vrana/adminer/master/designs/galkaev/adminer.css"=>"galkaev",
+			"https://raw.githack.com/Niyko/Hydra-Dark-Theme-for-Adminer/master/adminer.css"=>"hydra",
+			"https://raw.githack.com/vrana/adminer/master/designs/lucas-sandery/adminer.css"=>"lucas-sandery",
+			"https://raw.githack.com/vrana/adminer/master/designs/flat/adminer.css"=>"flat",
+			"https://raw.githack.com/vrana/adminer/master/designs/pappu687/adminer.css"=>"pappu687",
+			"https://raw.githack.com/vrana/adminer/master/designs/nicu/adminer.css"=>"nicu",
+			"https://raw.githack.com/vrana/adminer/master/designs/pokorny/adminer.css"=>"pokorny",
+			"https://raw.githack.com/vrana/adminer/master/designs/price/adminer.css"=>"price"
+		)
+);
